e562f15c82e98b6984dc86154d0f7b0a04baf4dc
JohnQ1981 <ikram1981@gmail.com>
2022-09-30 00:15:48 -0500

    added key and alt prop

--- packages/searchvideosapp/src/components/VideoList.js
+++ packages/searchvideosapp/src/components/VideoList.js
@@ -3,12 +3,12 @@ import VideoItem from "./VideoItem";
 
 const VideoList = ({ videos }) => {
   const renderedList = videos.map((video) => {
-    return <VideoItem video={video} />;
+    return <VideoItem key={video.snippet.channelId} video={video}  />;
   });
 
   //props.vidoes
 
-  return (<div className="ui relaxed divided list">{renderedList}</div>);
+  return <div className="ui relaxed divided list">{renderedList}</div>;
 };
 
 export default VideoList;
