c84f89eb569580007750f034934e59e70bdb5026
JohnQ1981 <ikram1981@gmail.com>
2022-09-29 22:46:56 -0500

    paths updated

--- packages/searchvideosapp/src/components/SearchBar.js
+++ packages/searchvideosapp/src/components/SearchBar.js
@@ -17,7 +17,7 @@ class SearchBar extends React.Component {
         <hr></hr>
         <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
-            <label>--Video Search--</label>
+            <label>---Video Search---</label>
             <input
               type="text"
               value={this.state.term}
